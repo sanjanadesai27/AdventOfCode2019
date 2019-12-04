657ae130e2b3ff9d79f7cf9ea783d91c9cfeb0c0
Sanjana Desai <sanjana.desai@shopify.com>
2019-12-04 15:43:59 -0800

    day 1 part 2

---
+++ fuelMass.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/ruby
+
+def calc(input) 
+  (input/3).floor - 2 
+end
+
+def findFuelMass(initialAmount)
+  totalFuel = calc(initialAmount).negative? ? 0 : calc(initialAmount)
+
+  if (totalFuel > 0 ) 
+    totalFuel += findFuelMass(totalFuel)
+  end 
+  totalFuel
+end 
+
+def main 
+  fuels = []
+  IO.foreach("./day1input.txt") { |amount|
+    fuels.push(findFuelMass(amount.to_i))
+  }
+  puts fuels.reduce(&:+)
+end
+
+main
+
+
